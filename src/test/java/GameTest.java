4eea998600e04a72a0b98a68f4ad78eb42582762
mey29 <mey29@pitt.edu>
2015-02-10 14:30:41 -0500

    Update GameTest.java

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -10,5 +10,59 @@ public class GameTest {
     assertEquals(0, 0);
   }
 
+  /*
+   * When the string is entered into doSomething(), the result should be the same
+   * for either lower or upper case of that string
+   */
+  @Test
+  public void caseInsensitiveCommands(){
+ 
+  //Create mock of Game object
+  Game tester = Mockito.mock(Game.class);
+  
+  //Test Cases
+  assertEquals(tester.doSomething("n"), tester.doSomething("N"));
+  assertEquals(tester.doSomething("s"), tester.doSomething("S"));
+  assertEquals(tester.doSomething("l"), tester.doSomething("L"));
+  assertEquals(tester.doSomething("i"), tester.doSomething("I"));
+  assertEquals(tester.doSomething("d"), tester.doSomething("D"));
+  assertEquals(tester.doSomething("h"), tester.doSomething("H"));
+ }
+   
+  /*
+   * When the string "n" or "N" is entered into doSomething(), a call to 
+   * moveNorth() should be made to increment _currentRoom by one
+   */
+  @Test
+   public void testMovementNorth(){
+     
+     //Create double of _currentRoom variable
+     int _currentRoom = 0;
+     
+     //Create mock of Game object
+     Game tester = Mockito.mock(Game.class);
+     
+     //Test Cases
+     assertEquals(tester.doSomething("n"), _currentRoom++);
+     assertEquals(tester.doSomething("N"), _currentRoom++); 
+   }
+   
+   /*
+    * When the string "s" or "S" is entered into doSomething(), a call to 
+    * moveSouth() should be made to decrement _currentRoom by one
+    */
+   @Test
+   public void testMovementSouth(){
+     //Create double of _currentRoom variable
+     int _currentRoom = 1;
+
+     //Create mock of Game object
+     Game tester = Mockito.mock(Game.class);
+     
+     //Test Cases
+     assertEquals(tester.doSomething("s"), _currentRoom--);
+     assertEquals(tester.doSomething("S"), _currentRoom--); 
+     
+   }
 }
 
