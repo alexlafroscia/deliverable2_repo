1b7cc7f434d94f127c6f57a2cde50c168e233169
mey29 <mey29@pitt.edu>
2015-02-10 14:29:48 -0500

    Update HouseTest.java

--- src/test/java/HouseTest.java
+++ src/test/java/HouseTest.java
@@ -9,6 +9,34 @@ public class HouseTest {
   public void HouseTestsRun() {
     assertEquals(0, 0);
   }
-
+  /*
+   * This test case checks that the expected output for a room that is less than zero
+   * or greater than _numRooms (6) produces the string 
+   * "You are in a magical land! But you are returned to the beginning!"
+   */
+  @Test
+  public void testBoundaryRooms(){
+    
+    //Create mock of House object
+    House tester = Mockito.mock(House.class);
+    
+    //Test Case
+    assertEquals(tester.getCurrentRoomInfo(), "You are in a magical land! But you are returned to the beginning!");
+  }
+  
+  /*
+   * This test case checks that array of rooms toReturn is of size 6  (_numRooms)
+   * The test case also checks the boolean values at each room
+   * northDoor, southDoor, hasCoffee, hasCream and hasSugar
+   */
+  @Test
+  public void testRoomGenerator(){
+    
+    House tester = new House(6);
+    
+    //Test Case
+    assertEquals(tester.generateRooms(6).length, 6);
+    
+  }
 }
 
