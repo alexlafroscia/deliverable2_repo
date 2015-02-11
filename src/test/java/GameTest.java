bad5c37602103dacf8309ddaa71374404e529b6a
Alex LaFroscia <alex@lafroscia.com>
2015-02-10 22:13:47 -0500

    Fix some compilation issues

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -18,13 +18,13 @@ public class GameTest {
    */
   @Test
   public void testCaseInsensitiveCommands(){
- 
-  //Create mock of player and house  
+
+  //Create mock of player and house
   Player mockPlayer = Mockito.mock(Player.class);
   House mockHouse = Mockito.mock(House.class);
   //Create Game object
   Game gameTester = new Game(mockPlayer, mockHouse);
-  
+
   //Test Cases
   assertEquals(gameTester.doSomething("n"), gameTester.doSomething("N"));
   assertEquals(gameTester.doSomething("s"), gameTester.doSomething("S"));
@@ -33,101 +33,101 @@ public class GameTest {
   assertEquals(gameTester.doSomething("d"), gameTester.doSomething("D"));
   assertEquals(gameTester.doSomething("h"), gameTester.doSomething("H"));
  }
-   
+
   /*
    * When the string "n" or "N" is entered into doSomething()
    * There should be a call to moveNorth()
-   * SHOULD FAIL DUE TO n NOT BEING RECOGNIZED 
+   * SHOULD FAIL DUE TO n NOT BEING RECOGNIZED
    */
   @Test
    public void testMovementNorth(){
-     
-     //Create mock of player and house  
+
+     //Create mock of player and house
      Player mockPlayer = Mockito.mock(Player.class);
      House mockHouse = Mockito.mock(House.class);
      //Create Game object
      Game gameTester = new Game(mockPlayer, mockHouse);
-     
+
      //Test Cases
      Mockito.verify(mockHouse, times(2)).moveNorth(); //Check if moveNorth() is called twice
      gameTester.doSomething("n");
      gameTester.doSomething("N");
    }
-   
+
    /*
-    * When the string "s" or "S" is entered into doSomething() 
+    * When the string "s" or "S" is entered into doSomething()
     * There should be a call to moveSouth()
     */
    @Test
    public void testMovementSouth(){
-     
-     //Create mock of player and house  
+
+     //Create mock of player and house
      Player mockPlayer = Mockito.mock(Player.class);
      House mockHouse = Mockito.mock(House.class);
      //Create Game object
      Game gameTester = new Game(mockPlayer, mockHouse);
-     
+
      //Test Cases
      Mockito.verify(mockHouse, times(2)).moveSouth(); //Check if moveSouth() is called twice
      gameTester.doSomething("s");
      gameTester.doSomething("S");
    }
-    
+
    /*
-    * When the string "l" or "L" is entered into doSomething() 
+    * When the string "l" or "L" is entered into doSomething()
     * There should be a call to look(player object, null)
     */
    @Test
    public void testMovementSouth(){
-     
-     //Create mock of player and house  
+
+     //Create mock of player and house
      Player mockPlayer = Mockito.mock(Player.class);
      House mockHouse = Mockito.mock(House.class);
      //Create Game object
      Game gameTester = new Game(mockPlayer, mockHouse);
-     
+
      //Test Cases
      Mockito.verify(mockHouse, times(2)).look(mockPlayer, null); //Check if look(player, null) is called twice
      gameTester.doSomething("l");
      gameTester.doSomething("L");
    }
-   
+
    /*
-    * When the string "i" or "I" is entered into doSomething() 
+    * When the string "i" or "I" is entered into doSomething()
     * There should be a call to showInventory()
     */
    @Test
    public void testMovementSouth(){
-     
-     //Create mock of player and house  
+
+     //Create mock of player and house
      Player mockPlayer = Mockito.mock(Player.class);
      House mockHouse = Mockito.mock(House.class);
      //Create Game object
      Game gameTester = new Game(mockPlayer, mockHouse);
-     
+
      //Test Cases
      Mockito.verify(mockPlayer, times(2)).showInventory(); //Check if showInventory() is called twice
      gameTester.doSomething("i");
      gameTester.doSomething("I");
    }
-   
+
    /*
-    * When the string "d" or "D" is entered into doSomething() 
+    * When the string "d" or "D" is entered into doSomething()
     * There should be a call to drink() which is stubbed to return true
     * Therefore boolean finalStatus will be true and toReturn will be set to 1
     */
    @Test
    public void testMovementSouth(){
-     
-     //Create mock of player and house  
+
+     //Create mock of player and house
      Player mockPlayer = Mockito.mock(Player.class);
      House mockHouse = Mockito.mock(House.class);
      //Create Game object
      Game gameTester = new Game(mockPlayer, mockHouse);
-     
+
      //Create stub of .drink() method in Player class
-     mockPlayer.when(mockPlayer.drink())thenReturn(true);
-     
+     mockPlayer.when(mockPlayer.drink()).thenReturn(true);
+
      //Test Cases
      assertEquals(gameTester.doSomething("d"), 1);
      assertEquals(gameTester.doSomething("D"), 1);
--- src/test/java/HouseTest.java
+++ src/test/java/HouseTest.java
@@ -5,12 +5,12 @@ import org.junit.*;
 import static org.mockito.Mockito.*;
 
 public class HouseTest {
-  
+
   @Test
   public void HouseTestsRun() {
     assertEquals(0, 0);
   }
-  
+
   /*
    * This test case checks that when getCurrentRoomInfo() is called
    * a description is returned for a valid _currentRoom that is found
@@ -18,59 +18,59 @@ public class HouseTest {
    */
   @Test
   public void testGetCurrentRoomInfo(){
-    
+
     //Create mock of Room class
     Room mockRoom = Mockito.mock(Room.class);
     mockRoom[] mockRoomArray = new mockRoom[1];//Create array of mockRooms set to size 1
-    
+
     //Create stub of .getDescription() method in Room class
-    mockRoom.when(mockRoom.getDescription())thenReturn("hi");
-    
+    mockRoom.when(mockRoom.getDescription()).thenReturn("hi");
+
     //Create House object that sets _numRooms to 1 and _rooms to mockRoomArray and _currentRoom to 0
     House houseTester = new House(mockRoomArray);
-    
+
     //Test Case
     assertEquals(houseTester.getCurrentRoomInfo(), "hi");
   }
-  
+
   /*
-   * This test case checks that the expected output for a room 
+   * This test case checks that the expected output for a room
    * greater than the size of the array returns
    * "You are in a magical land! But you are returned to the beginning!"
    */
   @Test
   public void testGetCurrentRoomInfoEdgeCase(){
-    
+
     //Create mock of Room class
     Room mockRoom = Mockito.mock(Room.class);
     mockRoom[] mockRoomArray = new mockRoom[1];//Create array of mockRooms set to size 1
-    
+
     //Create House object that sets _numRooms to 1 and _rooms to mockRoomArray and _currentRoom to 0
     House houseTester = new House(mockRoomArray);
-        
+
     //Set _currentRoom to 1, thus outside size of mockRoomArray
     houseTester.moveNorth();
-    
+
     //Test Case
     assertEquals(houseTester.getCurrentRoomInfo(), "You are in a magical land! But you are returned to the beginning!");
   }
-  
+
   /*
    * This test case checks that toReturn room array contains the correct boolean values
-   * Associated with the room's position in the house that is the first room where the cream 
+   * Associated with the room's position in the house that is the first room where the cream
    * Is found and there is only a north door
    */
   @Test
   public void testRoomGenerator(){
-    
+
     //Create mock of Room class
     Room mockRoom = Mockito.mock(Room.class);
     mockRoom[] mockRoomArray = new mockRoom[1];//Create array of mockRooms set to size 1
-    
+
     //Create House object that sets _numRooms to 1 and _rooms to mockRoomArray and _currentRoom to 0
     House houseTester = new House(mockRoomArray);
-    
+
     //Test Case
-    assertEquals(roomGenerated, new mockRoom[]{false, true, false, true, false}); 
+    assertEquals(roomGenerated, new mockRoom[]{false, true, false, true, false});
   }
 }
