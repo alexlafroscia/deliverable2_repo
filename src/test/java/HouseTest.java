de845d6ada23d5f8125d1ea8b0f279497535072f
mey29 <mey29@pitt.edu>
2015-02-10 21:54:13 -0500

    The new House test file... might need one more?

--- src/test/java/HouseTest.java
+++ src/test/java/HouseTest.java
@@ -2,41 +2,75 @@ package com.laboon;
 
 import static org.junit.Assert.*;
 import org.junit.*;
+import static org.mockito.Mockito.*;
 
 public class HouseTest {
-
+  
   @Test
   public void HouseTestsRun() {
     assertEquals(0, 0);
   }
+  
   /*
-   * This test case checks that the expected output for a room that is less than zero
-   * or greater than _numRooms (6) produces the string 
-   * "You are in a magical land! But you are returned to the beginning!"
+   * This test case checks that when getCurrentRoomInfo() is called
+   * a description is returned for a valid _currentRoom that is found
+   * within the array
    */
   @Test
-  public void testBoundaryRooms(){
+  public void testGetCurrentRoomInfo(){
     
-    //Create mock of House object
-    House tester = Mockito.mock(House.class);
+    //Create mock of Room class
+    Room mockRoom = Mockito.mock(Room.class);
+    mockRoom[] mockRoomArray = new mockRoom[1];//Create array of mockRooms set to size 1
+    
+    //Create stub of .getDescription() method in Room class
+    mockRoom.when(mockRoom.getDescription())thenReturn("hi");
+    
+    //Create House object that sets _numRooms to 1 and _rooms to mockRoomArray and _currentRoom to 0
+    House houseTester = new House(mockRoomArray);
     
     //Test Case
-    assertEquals(tester.getCurrentRoomInfo(), "You are in a magical land! But you are returned to the beginning!");
+    assertEquals(houseTester.getCurrentRoomInfo(), "hi");
   }
   
   /*
-   * This test case checks that array of rooms toReturn is of size 6  (_numRooms)
-   * The test case also checks the boolean values at each room
-   * northDoor, southDoor, hasCoffee, hasCream and hasSugar
+   * This test case checks that the expected output for a room 
+   * greater than the size of the array returns
+   * "You are in a magical land! But you are returned to the beginning!"
+   */
+  @Test
+  public void testGetCurrentRoomInfoEdgeCase(){
+    
+    //Create mock of Room class
+    Room mockRoom = Mockito.mock(Room.class);
+    mockRoom[] mockRoomArray = new mockRoom[1];//Create array of mockRooms set to size 1
+    
+    //Create House object that sets _numRooms to 1 and _rooms to mockRoomArray and _currentRoom to 0
+    House houseTester = new House(mockRoomArray);
+        
+    //Set _currentRoom to 1, thus outside size of mockRoomArray
+    houseTester.moveNorth();
+    
+    //Test Case
+    assertEquals(houseTester.getCurrentRoomInfo(), "You are in a magical land! But you are returned to the beginning!");
+  }
+  
+  /*
+   * This test case checks that toReturn room array contains the correct boolean values
+   * Associated with the room's position in the house that is the first room where the cream 
+   * Is found and there is only a north door
    */
   @Test
   public void testRoomGenerator(){
     
-    House tester = new House(6);
+    //Create mock of Room class
+    Room mockRoom = Mockito.mock(Room.class);
+    mockRoom[] mockRoomArray = new mockRoom[1];//Create array of mockRooms set to size 1
+    
+    //Create House object that sets _numRooms to 1 and _rooms to mockRoomArray and _currentRoom to 0
+    House houseTester = new House(mockRoomArray);
     
     //Test Case
-    assertEquals(tester.generateRooms(6).length, 6);
-    
+    assertEquals(roomGenerated, new mockRoom[]{false, true, false, true, false}); 
   }
 }
-
