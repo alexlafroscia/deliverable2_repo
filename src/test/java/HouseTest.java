848421b028f94157d7e821b085b97d55d2c1f4c5
mey29 <mey29@pitt.edu>
2015-02-12 10:22:23 -0500

    Done!

--- src/test/java/HouseTest.java
+++ src/test/java/HouseTest.java
@@ -64,17 +64,25 @@ public class HouseTest {
    * Is found and there is only a north door
    */
   @Test
-  public void testRoomGenerator(){
-
-    Room[] roomArray = new Room[1];//Create array of rooms set to size 1
-
+  public void testRoomGenerator(){    
     //Create House object that sets _numRooms to 1 and _rooms to mockRoomArray and _currentRoom to 0
-    House houseTester = new House(roomArray);
-
-    Room comparison = new Room(false, true, false, true, false);
+    House houseTester = new House(2);
+                            //coffee, cream, sugar, north, south
+    Room comparison = new Room(false, true, true, false, false); //For 0th room
+    Room comparison2 = new Room(false, false, true, false, true); //For 1st room
 
     //Test Case
-    assertEquals(houseTester.generateRooms(1)[0], comparison);
+    assertEquals(houseTester.generateRooms(1)[0].hasCream(), comparison.hasCream());
+    assertEquals(houseTester.generateRooms(1)[0].hasSugar(), comparison.hasSugar());
+    assertEquals(houseTester.generateRooms(1)[0].hasCoffee(), comparison.hasCoffee());
+    assertEquals(houseTester.generateRooms(1)[0].northExit(), comparison.northExit());
+    assertEquals(houseTester.generateRooms(1)[0].southExit(), comparison.southExit());
+    
+    assertEquals(houseTester.generateRooms(2)[1].hasCream(), comparison2.hasCream());
+    assertEquals(houseTester.generateRooms(2)[1].hasSugar(), comparison2.hasSugar());
+    assertEquals(houseTester.generateRooms(2)[1].hasCoffee(), comparison2.hasCoffee());
+    assertEquals(houseTester.generateRooms(2)[1].northExit(), comparison2.northExit());
+    assertEquals(houseTester.generateRooms(2)[1].southExit(), comparison2.southExit());
   }
 }
 
